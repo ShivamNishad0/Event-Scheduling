d1014d809d1a50a36bec4c5d6ba680d6ef0ea359
shivamnishad0 <2k23.mca2313672@gmail.com>
2025-11-10 17:15:52 +0530

    deploye the project on the render

--- server/index.js
+++ server/index.js
@@ -10,7 +10,8 @@ const port = process.env.PORT || 5001;
 
 // Middleware
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  // res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'https://event-scheduling.onrender.com');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
